f613810c3aaaaebeea363ed6d828f7be0e453b3e
dmitry <harldmitr@gmail.com>
2018-05-06 20:39:05 +0300

    added logging with glog

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@
 
 int main() {
     google::InitGoogleLogging("log.txt");
+    FLAGS_logtostderr = 0;
     FLAGS_stderrthreshold = 0;
+    FLAGS_log_prefix = 0;
     LOG(INFO) << "logging started";
-    std::string filename = "/Users/lopatkindaniil/CLionProjects/riscv-simulator/test/a.out";
+    std::string filename = "../test/a.out";
     Processor processor;
     processor.process(filename);
     return 0;
