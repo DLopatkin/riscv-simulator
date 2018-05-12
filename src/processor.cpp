14d6ea34c11310c7cf831fec18fa8094b3bba871
dmitry <harldmitr@gmail.com>
2018-05-12 18:30:59 +0300

    added linear block processing

--- src/processor.cpp
+++ src/processor.cpp
@@ -7,6 +7,7 @@
 #include "processor.h"
 #include "context.h"
 #include <vector>
+#include <map>
 #include "elf_reader.h"
 #include "memory.h"
 
@@ -21,12 +22,50 @@ int Processor::process(std::string filename) {
 
     hart.PC = elfReader.GetPC();
 
+
+    auto linear_block = new std::vector<Instruction>();;
+    auto instr_cache = new std::map<uint32_t, std::vector<Instruction> *>();
+    instr_cache->insert(std::make_pair(hart.PC, linear_block));
+
+    Instruction instr = Decoder::decode(mem->load_word(hart.PC));
+    linear_block->push_back(instr);
+
+    Instr_impl::execute(instr, hart, *mem);
+
+    if(instr.opcode < JAL || instr.opcode > BGEU)
+        hart.PC += 4;
+
+
     while(hart.PC != 0) {
-        Instruction instr = Decoder::decode(mem->load_word(hart.PC));
 
-        Instr_impl::execute(instr, hart, *mem);
+        if (instr_cache->find(hart.PC) != instr_cache->end()) {
+            linear_block = (*instr_cache)[hart.PC];
+            for (Instruction instr : *linear_block)
+            {
+                Instr_impl::execute(instr, hart, *mem);
+                if(instr.opcode < JAL || instr.opcode > BGEU)
+                    hart.PC += 4;
+            }
 
-        if(instr.opcode < JAL || instr.opcode > BGEU)
-            hart.PC += 4;
+            linear_block = new std::vector<Instruction>();
+            //instr_cache[hart.PC] = linear_block;
+            instr_cache->insert(std::make_pair(hart.PC, linear_block));
+
+        }
+        else
+        {
+            Instruction instr = Decoder::decode(mem->load_word(hart.PC));
+            linear_block->push_back(instr);
+
+            Instr_impl::execute(instr, hart, *mem);
+
+            if(instr.opcode < JAL || instr.opcode > BGEU)
+                hart.PC += 4;
+
+
+        }
+
+
+
     }
 }
