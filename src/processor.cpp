fc8b29fc321b3810a27009a7838efa153837925b
Daniil Lopatkin <daniil199525@gmail.com>
2018-05-12 20:35:50 +0300

    Linear blocks fix

--- src/processor.cpp
+++ src/processor.cpp
@@ -8,6 +8,7 @@
 #include "context.h"
 #include <vector>
 #include <map>
+#include <glog/logging.h>
 #include "elf_reader.h"
 #include "memory.h"
 
@@ -25,44 +26,36 @@ int Processor::process(std::string filename) {
 
     auto linear_block = new std::vector<Instruction>();;
     auto instr_cache = new std::map<uint32_t, std::vector<Instruction> *>();
-    instr_cache->insert(std::make_pair(hart.PC, linear_block));
 
-    Instruction instr = Decoder::decode(mem->load_word(hart.PC));
-    linear_block->push_back(instr);
-
-    Instr_impl::execute(instr, hart, *mem);
-
-    if(instr.opcode < JAL || instr.opcode > BGEU)
-        hart.PC += 4;
-
+    uint32_t head_PC = hart.PC;
 
     while(hart.PC != 0) {
 
         if (instr_cache->find(hart.PC) != instr_cache->end()) {
             linear_block = (*instr_cache)[hart.PC];
+            LOG(WARNING) << "Linear block: PC:" << std::hex << hart.PC << " LEN: " << linear_block->size();
             for (Instruction instr : *linear_block)
             {
                 Instr_impl::execute(instr, hart, *mem);
                 if(instr.opcode < JAL || instr.opcode > BGEU)
                     hart.PC += 4;
             }
-
-            linear_block = new std::vector<Instruction>();
-            //instr_cache[hart.PC] = linear_block;
-            instr_cache->insert(std::make_pair(hart.PC, linear_block));
-
         }
         else
         {
+            LOG(WARNING) << "Decode";
             Instruction instr = Decoder::decode(mem->load_word(hart.PC));
             linear_block->push_back(instr);
 
             Instr_impl::execute(instr, hart, *mem);
 
-            if(instr.opcode < JAL || instr.opcode > BGEU)
+            if(instr.opcode < JAL || instr.opcode > BGEU){
                 hart.PC += 4;
-
-
+            } else {
+                instr_cache->insert(std::make_pair(head_PC, linear_block));
+                head_PC = hart.PC;
+                linear_block = new std::vector<Instruction>();
+            }
         }
 
 
